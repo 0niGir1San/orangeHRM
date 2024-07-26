733a883a67fa63006124abffb3368656ae5d3f4a
0niGir1San <onegirisan3@gmail.com>
2024-07-27 00:53:18 +0600

    Low

--- src/main/java/com/orange/drivers/OperaWebDriver.java
+++ src/main/java/com/orange/drivers/OperaWebDriver.java
@@ -1,8 +1,8 @@
 package com.orange.drivers;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.managers.OperaDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.opera.OperaDriver;
 
 import java.time.Duration;
 
@@ -10,7 +10,7 @@ public class OperaWebDriver {
 
     public static WebDriver loadOperaDriver(){
         WebDriverManager.operadriver().setup(); // Установка драйвера через WebDriverManager
-        WebDriver driver = new OperaDriver(); // Создание экземпляра OperaDriver
+        WebDriver driver = (WebDriver) new OperaDriverManager(); // Создание экземпляра OperaDriver
         driver.manage().window().maximize(); // Максимизация окна браузера
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15)); // Установка времени ожидания
         return driver;
