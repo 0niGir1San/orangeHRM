42a24da7f07947bf493c33104bb982283fb21f74
0niGir1San <onegirisan3@gmail.com>
2024-07-26 16:24:21 +0600

    server

--- src/main/java/com/orange/drivers/ChromeWebDriver.java
+++ src/main/java/com/orange/drivers/ChromeWebDriver.java
@@ -5,7 +5,9 @@ import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
 import java.time.Duration;
+
 import static com.orange.utils.ConfigReader.getValue;
 
 public class ChromeWebDriver {
@@ -15,15 +17,18 @@ public class ChromeWebDriver {
 
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*"); // Для запуска из удаленного сервера
-        options.addArguments("--disable-extensions"); //
-        options.addArguments("--window-size-1920,1080"); // Расширение экрана
-        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-extensions"); // Отключить расширения
+        options.addArguments("--window-size=1920,1080"); // Размер окна
+        options.addArguments("--no-sandbox"); // Отключить песочницу
+        options.addArguments("--disable-dev-shm-usage"); // Отключить shared memory
         options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
 
-        if(Boolean.parseBoolean(getValue("headless"))) {
+        // Проверка, если указано значение headless в конфигурации
+        if (Boolean.parseBoolean(getValue("headless"))) {
             options.addArguments("--headless");
         }
-        WebDriver driver = new ChromeDriver();
+
+        WebDriver driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         return driver;
